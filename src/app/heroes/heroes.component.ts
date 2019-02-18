9d2d7cb0f3cf1d85dd3ed1fc1406f2de92ae0cce
robertlluberes <robertlluberes@gmail.com>
2019-02-18 15:19:06 -0400

    Now HeroService is asynchronous

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -24,7 +24,7 @@ export class HeroesComponent implements OnInit {
   }
 
   getHeroes():void{
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes().subscribe(hero => this.heroes = hero);
   }
 
 }
