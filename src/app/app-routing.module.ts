aa04396cece6531f21e6eb07c88e00a329937849
robertlluberes <robertlluberes@gmail.com>
2019-02-21 14:36:27 -0400

    Was added the app-routing and a route for the HeroesComponent

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { HeroesComponent } from './heroes/heroes.component';
+import { NgModule }             from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: 'heroes', component: HeroesComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [ RouterModule ]  
+})
+
+export class AppRoutingModule {}
