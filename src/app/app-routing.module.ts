5f5e96b624b9cfeb7e52b105d47b12e73bc59bc4
robertlluberes <robertlluberes@gmail.com>
2019-02-22 13:40:54 -0400

    Was added a parametrized path route for HeroDetailComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
@@ -6,7 +7,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 const routes: Routes = [
   { path: 'heroes', component: HeroesComponent },
   { path: 'dashboard', component: DashboardComponent},
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full'}
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  { path: 'detail/:id', component: HeroDetailComponent }
 ];
 
 @NgModule({
